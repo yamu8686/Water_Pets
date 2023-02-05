1cbd367003e1a8902b8a8fba198426289c7fb7ee
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-02-05 04:43:05 +0000

    [Add]creatures

--- app/controllers/public/creatures_controller.rb
+++ app/controllers/public/creatures_controller.rb
@@ -1,10 +1,30 @@
 class Public::CreaturesController < ApplicationController
   def index
+    @creature = Creture.new
+    @creatures = Creature.all
+    @user = current_user
   end
-
+  
+  def create
+    @creature = Creature.new(creature_params)
+    @creature.user.id = currect_user.id
+    if @creature.save
+      flash[:notice] = "投稿に成功しました"
+      redirect_to creature_path(@creature)
+    else
+      flash[:alert]
+      @creatures = Creature.all
+      render :index
+    end
+  end
+  
   def show
+    @creatures = Creature.all
+    @creature = Creature.find(params[:id])
+    @user = current_user
   end
 
   def edit
+    
   end
 end
