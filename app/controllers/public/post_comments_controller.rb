f3d900f9a348520b790ce3413fcaa9d036878973
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-02-19 15:34:46 +0000

    非同期通信途中

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -2,15 +2,14 @@ class Public::PostCommentsController < ApplicationController
 
   def create
     creature = Creature.find(params[:creature_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.creature_id = creature.id
-    comment.save
-    redirect_to public_creature_path(creature)
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.creature_id = creature.id
+    @comment.save
   end
 
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to public_creature_path(params[:creature_id])
+    @comment = PostComment.find(params[:id])
+    @comment.destroy
   end
 
   private
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,4 +1,6 @@
 class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :creature
+
+  validates :content, presence: true
 end
