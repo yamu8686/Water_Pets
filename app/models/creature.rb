87fa1b6b1f592d5bd671232e00b23820bf3f2066
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-02-25 06:32:15 +0000

    update

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -19,6 +19,6 @@ class Creature < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-      image.variant(resize_to_limit: [width,height]).processed
+    image.variant(resize_to_limit: [width,height]).processed
   end
 end
